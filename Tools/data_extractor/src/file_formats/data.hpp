2bd7088ceefec5f95c1fbd0036298fb7207069aa
Schweini07 <schweinthaler@gmx.at>
2024-05-15 14:25:26 +0200

    Clean up data class

--- Tools/data_extractor/src/file_formats/data.hpp
+++ Tools/data_extractor/src/file_formats/data.hpp
@@ -16,16 +16,7 @@ public:
     void GetDataBuffer(std::vector<uint8_t> &data_buffer, uint32_t offset, uint32_t buffer_size);
     uint32_t GetFileSize();
 
-    void ExtractDataFiles(Dict *dict);
-    void RepackDataFiles(Dict *dict);
-
-    bool is_compressed;
-
 private:
-    void ExtractDataFile(FileSection &data_file);
-    void DecompressDataFile(FileSection &data_file);
-    void CompressDataFile(FileSection &data_file);
-
     uint32_t file_size;
 
     std::string file_path;
